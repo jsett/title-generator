f629fd70ec66056bd3dbc78ce1b7ce742ab3c4df
John <28509298+jsett@users.noreply.github.com>
2017-12-27 14:39:20 -0500

    feat: data controls are working.

--- js/myapp.js
+++ js/myapp.js
@@ -6,11 +6,38 @@ app.controller('myCtrl', function($scope, $http) {
 
 var app = angular.module('myApp', ['ui.bootstrap']);
 app.controller('DropdownCtrl', function ($scope, $log) {
-  $scope.items = [
-    'The first choice!',
-    'And another choice for you.',
-    'but wait! A third!'
-  ];
+  $scope.mydata = {
+    pets: ["Dog","Cat","Cow","Bird","Goat","Fish"],
+    days: ["Mon","Tue","Wed","Thur","Fri","Sat","Sun"]
+  };
+  $scope.adders = {};
+  //$scope.myitems = ["Dog","Cat","Cow","Bird","Goat","Fish"];
+
+  $scope.mydatakeys = function()
+  {
+    return Object.keys($scope.mydata);
+  }
+  $scope.removeItem = function(k,x){
+    $scope.mydata[k].splice($scope.mydata[k].indexOf(x),1);
+    alert(x+" removed");
+  };
+  $scope.addItem = function(k){
+    if ($scope.adders[k] != "")
+    {
+      $scope.mydata[k].push($scope.adders[k]);
+      $scope.adders[k] = "";
+    }
+  };
+  $scope.removeGroup = function(k){
+    delete $scope.mydata[k]
+  }
+  $scope.addGroup = function(){
+    if ($scope.groupdata != "")
+    {
+      $scope.mydata[$scope.groupdata] = []
+      $scope.groupdata = "";
+    }
+  }
 
   $scope.status = {
     isopen: false
